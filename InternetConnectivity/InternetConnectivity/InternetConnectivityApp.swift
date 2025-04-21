9e4de07a3210fa9425b76e4c3b5f48319d8f3ada
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-04-21 09:59:22 +0200

    Create project

---
+++ InternetConnectivity/InternetConnectivity/InternetConnectivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternetConnectivityApp.swift
+//  InternetConnectivity
+//
+//  Created by Jesus Antonio Gil on 21/4/25.
+//
+
+import SwiftUI
+
+@main
+struct InternetConnectivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InternetConnectivity/InternetConnectivityTests/InternetConnectivityTests.swift
@@ -0,0 +1,17 @@
+//
+//  InternetConnectivityTests.swift
+//  InternetConnectivityTests
+//
+//  Created by Jesus Antonio Gil on 21/4/25.
+//
+
+import Testing
+@testable import InternetConnectivity
+
+struct InternetConnectivityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
